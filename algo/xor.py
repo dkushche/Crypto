3e7732af1ea0053395a1578689bd8e8897cb0a83
Dimka <d.kushchevskyi@sirinsoftware.com>
2020-02-10 18:32:34 +0200

    Some troubles in xor decode

--- algo/xor.py
+++ algo/xor.py
@@ -1,6 +1,11 @@
-def xor(data, key):
-    return "Nothing xor"
+from itertools import cycle
 
+def xor(data, key):
+    key_bytes = cycle(bytearray(key, "utf-8"))
+    data_in_bytes = bytearray(data, "utf-8")
+    result_bytes = bytearray(a ^ b for a, b in zip(data_in_bytes, key_bytes))
+    result_str = result_bytes.decode("utf-8")
+    return result_str
 
 def hack_xor(data):
     print("Error: You can't hack this algo")
