92187764317f74e9b21047dd34971a20d117f2e6
Dimka <dima.kushhevskij@gmail.com>
2020-09-20 20:44:26 +0300

    Add module example

---
+++ algo_storage/module_example.py
@@ -0,0 +1,25 @@
+import crypto_tools
+
+
+def module_little_doc():
+    return "little_doc"
+
+
+def module_full_doc():
+    return """
+    full_doc
+    """
+
+
+def module_processing(data, input_val):
+    return result
+
+
+@crypto_tools.file_manipulation
+def module(data):
+    input_val = int(crypto_tools.cterm('input','data', 'ans'))
+    return hash_processing(data, input_val)
+
+
+module.little_doc = module_little_doc
+module.full_doc = module_full_doc
