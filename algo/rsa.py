3f24e5ff40919145dbd7e10cb5210e1dfb9538ea
Dimka <dima.kushhevskij@gmail.com>
2020-09-26 20:38:06 +0300

    Improve RSA

--- algo/rsa.py
+++ algo/rsa.py
@@ -1,3 +1,4 @@
+from bitarray import bitarray
 import crypto_tools
 import math
 
@@ -9,33 +10,65 @@ def rsa_little_doc():
 def rsa_full_doc():
     return """
         RSA quite easy, but still effective and elegant;
-        I love it.
+        I love it. Block size in bits
     """
 
 
-def rsa_processing(data, p_value, q_value, e_value, m_value, encrypt):
+def get_closed_key(p_value, q_value, e_value):
+    closed_euler = (p_value - 1) * (q_value - 1)
+    gcd, _, _ = crypto_tools.EGCD(e_value, closed_euler)
+    if gcd != 1:
+            raise ValueError(f"GCD of E and P * Q == {gcd}")
+    m_value = 1
+    d_value = (m_value * closed_euler + 1) / e_value
+    while not d_value.is_integer():
+        m_value += 1
+        d_value = (m_value * closed_euler + 1) / e_value
+    return int(d_value)
+
+
+def get_block_sizes(encrypt, encrypt_block_size, byte_buf):
+    if encrypt == "encrypt":
+        data_block_size = encrypt_block_size
+        res_block_size = byte_buf * 8
+    else:
+        data_block_size = byte_buf * 8
+        res_block_size = encrypt_block_size
+    return data_block_size, res_block_size
+
+def rsa_processing(data, p_value, q_value, e_value, encrypt):
     if crypto_tools.is_prime(p_value) and crypto_tools.is_prime(q_value):
         open_mix = p_value * q_value
-        closed_euler = (p_value - 1) * (q_value - 1)
-        gcd, _, _ = crypto_tools.EGCD(e_value, closed_euler)
-        if gcd != 1:
-            raise ValueError(f"GCD of E and P * Q == {gcd}")
-        d_value = (m_value * closed_euler + 1) // e_value
-        block_size = int(math.log2(open_mix))
-        if block_size // 8 < 1:
-            raise ValueError(f"P * Q needs to be bigger 2^8 but it {open_mix}")
-        crypto_tools.supl_to_mult(data, block_size)
+        d_value = get_closed_key(p_value, q_value, e_value)
+
+        encrypt_block_size = int(math.log2(open_mix))
+        byte_buf = math.ceil(encrypt_block_size / 8)
+
+        data_block_size, res_block_size = get_block_sizes(
+            encrypt, encrypt_block_size, byte_buf
+        )
         key = e_value if encrypt == "encrypt" else d_value
-        result = bytearray()
-        for i in range(0, len(data), block_size // 8):
-            block_val = int.from_bytes(data[i: i + block_size // 8], "big")
-            assert 0 <= block_val < pow(2, block_size) - 1
-            result += (pow(block_val, key) % open_mix).to_bytes(
-                        block_size // 8, "big")
-        if encrypt == "encrypt":
-            return result
-        else:
-            return result.decode()
+        crypto_tools.supl_to_mult(data, data_block_size)
+
+        result = bitarray()
+        for i in range(0, len(data), data_block_size):
+            block = bitarray()
+            if encrypt == "encrypt":
+                block += bitarray('0') * (res_block_size - data_block_size)
+            block += data[i: i + data_block_size]
+
+            block_val = int.from_bytes(block, "big")
+            res_val = (pow(block_val, key) % open_mix).to_bytes(
+                        byte_buf, "big")
+
+            block = bitarray()
+            block.frombytes(res_val)
+            result += block[-res_block_size:]
+
+        if len(result) % 8:
+            result = result[:-(len(result) % 8)]
+        result = bytes(result)
+        return result if encrypt == "encrypt" else result.decode()
     else:
         raise ValueError("P and Q value need to be prime")
     return result
@@ -43,21 +76,24 @@ def rsa_processing(data, p_value, q_value, e_value, m_value, encrypt):
 
 @crypto_tools.file_manipulation
 def rsa(data):
-    if data.__class__ == str:
-        data = bytearray(data.encode())
+    buf = data
+    data = bitarray()
+    if buf.__class__ == str:
+        data.frombytes(buf.encode())
+    else:
+        data.frombytes(buf)
+
     p_value = int(crypto_tools.cterm('input',
                                      'Enter first(p) number: ', 'ans'))
     q_value = int(crypto_tools.cterm('input',
                                      'Enter second(q) number: ', 'ans'))
     e_value = int(crypto_tools.cterm('input',
                                      'Enter open(e) number: ', 'ans'))
-    m_value = int(crypto_tools.cterm('input',
-                                     'Enter multiplier(M) number: ', 'ans'))
     encrypt = crypto_tools.cterm('input',
                                  'You want encrypt or decrypt: ', 'ans')
     if encrypt != "encrypt" and encrypt != "decrypt":
         raise ValueError(f"Incorrect action {encrypt}")
-    return rsa_processing(data, p_value, q_value, e_value, m_value, encrypt)
+    return rsa_processing(data, p_value, q_value, e_value, encrypt)
 
 
 rsa.little_doc = rsa_little_doc
