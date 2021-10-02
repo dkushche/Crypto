43f469c6f47d0e42177e29c4eb8efbb0711bb407
Dimka Kushchevskyi <dima.kushhevskij@gmail.com>
2021-10-02 12:16:20 +0300

    Fix lfsr

--- algo/lfsr.py
+++ algo/lfsr.py
@@ -10,74 +10,85 @@ def lfsr_little_doc():
 
 def lfsr_full_doc():
     return """
+    register size:
+        size of linear feedback shift register in bytes
     executive polynomial examples:
-        [11, 2, 0], [30, 6, 4, 1, 0] [24, 4, 3, 1, 0]
+        [11, 2, 0]
+    start register examples:
+        [0, 3]
     """
 
-"""
-Rewrite it using generator.
-"""
-def lfsr_tick(key):
-    next_val = 0
 
-    for val in key:
-        next_val ^= val
-    
-    prev_bit = key[0]
-    for i in range(1, len(key)):
-        buf = key[i]
-        key[i] = prev_bit
-        prev_bit = buf
+def lfsr_tick():
+    register_size = yield
+    start_state = yield
+    exec_xor_pos = yield
 
-    key[0] = next_val
+    register = bitarray()
+    register.frombytes(bytes(register_size))
+    for pos in start_state:
+        register[pos] = 1
 
-    return key
+    while True:
+        random_number = bitarray()
+
+        for i in range(8):
+            updated_bit = 0
+            for pos in exec_xor_pos:
+                updated_bit ^= register[pos]
+
+            random_number.append(register[0])
+
+            for j in range(register_size * 8 - 1):
+                register[j] = register[j + 1]
+
+            register[register_size * 8 - 1] = updated_bit
+
+        yield random_number
 
 
-def key_from_poly(exec_poly):
-    bytes_amount = ceil((max(exec_poly) + 1) / 8)
-    key = bitarray()
-    key.frombytes(bytes(bytes_amount))
-
-    for val in exec_poly:
-        key[len(key) - 1 - val] = 1
-
-    return key
-
-"""
-Have no time refactor later
-"""
-def lfsr_processing(data, exec_poly):
+def lfsr_processing(data, register_size, exec_xor_pos, start_state):
     if data.__class__ == str:
         data = bytearray(data, "utf-8")
-    key = key_from_poly(exec_poly)
+
+    if register_size * 8 < max(exec_xor_pos) or register_size * 8 < max(start_state):
+        raise ValueError(f"{register_size} smaller then significant bit position {max(max(exec_xor_pos), max(start_state))}")
+
+    tick = lfsr_tick()
+
+    next(tick)
+    tick.send(register_size)
+    tick.send(start_state)
+    tick.send(exec_xor_pos)
+
+    crypto_tools.supl_to_mult(data, register_size)
 
     result = bytearray()
 
-    crypto_tools.supl_to_mult(data, len(bytes(key)))
-    data_pos = 0
-    key_pos = 0
-    while data_pos != len(data):
-        
-        if key_pos == len(bytes(key)):
-            key_pos = 0
-            lfsr_tick(key)
+    for byte in data:
+        random_number = next(tick)
+        random_number = int.from_bytes(random_number, byteorder='big', signed=False)
 
-        result.append(data[data_pos] ^ bytes(key)[key_pos])
-
-        key_pos += 1
-        data_pos += 1    
+        result.append(byte ^ random_number)
 
     return result
 
 
 @crypto_tools.file_manipulation()
 def lfsr(data):
-    exec_poly = json.loads(crypto_tools.cterm('input', 'Enter executive polynomial(list): ', 'ans'))
-    if exec_poly.__class__ != list:
-        raise ValueError(f"Incorrect exec_poly class {exec_poly.__class__}")
+    register_size = int(crypto_tools.cterm('input', 'Enter register_size(uint): ', 'ans'))
+    if register_size <= 0:
+        raise ValueError("Register size must be bigger then 0")
 
-    return lfsr_processing(data, exec_poly)
+    start_state = json.loads(crypto_tools.cterm('input', 'Enter register start state(list): ', 'ans'))
+    if start_state.__class__ != list:
+        raise ValueError(f"Incorrect exec_xor_pos class {start_state.__class__}")
+
+    exec_xor_pos = json.loads(crypto_tools.cterm('input', 'Enter executive polynomial xor bits(list): ', 'ans'))
+    if exec_xor_pos.__class__ != list:
+        raise ValueError(f"Incorrect exec_xor_pos class {exec_xor_pos.__class__}")
+
+    return lfsr_processing(data, register_size, exec_xor_pos, start_state)
 
 
 lfsr.little_doc = lfsr_little_doc
