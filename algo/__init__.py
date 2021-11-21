e94003ff9c27f6db91dc97a898a6eec454216cd0
Dmytro Kushchevskyi <dima.kushhevskij@gmail.com>
2021-11-21 14:38:27 +0200

    Add cork for ms_cryptoapi

--- algo/__init__.py
+++ algo/__init__.py
@@ -1,3 +1,5 @@
+import platform
+
 from .lfsr_generator import lfsr_generator
 from .diffie_hellman import diffie_hellman
 from .ansi_x9_17 import ansi_x9_17
@@ -18,6 +20,9 @@ from .xor import xor
 from .rc4 import rc4
 from .rsa import rsa
 from .dsa import dsa
+
+if platform.system() == "Windows":
+    from .ms_cryptoapi import ms_cryptoapi
 
 def __doc__():
     return f"Cryptography module"
