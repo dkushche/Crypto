2935fda6c363fd2e73b55d2a7bd2f2e84b6a03cb
Dmytro Kushchevskyi <d.kushchevskyi@sirinsoftware.com>
2022-02-08 00:29:49 +0200

    Add install stage

--- executor.py
+++ executor.py
@@ -8,7 +8,7 @@ if platform.system() == "Linux":
     sys.argv[0] = f"linux_platform/{sys.argv[0][2:]}"
     subprocess.call(sys.argv)
 elif platform.system() == "Windows":
-    sys.argv[0] = f"windows_platform/{sys.argv[0][2:]}.bat"
+    sys.argv[0] = f"windows_platform\{sys.argv[0]}.bat"
     subprocess.call(sys.argv)
 else:
     print("Error: Unknown platform")
--- windows_platform/install.bat
+++ windows_platform/install.bat
@@ -1,9 +1,11 @@
 @ECHO "Start installation process"
 
-@python -m venv crypto_env
+python -m venv crypto_env
 
-@crypto_env\Scripts\activate.bat
-@pip install -r windows_platform\requirements.txt
-@crypto_env\Scripts\deactivate.bat
+call crypto_env\Scripts\activate.bat
+
+pip install -r windows_platform\requirements.txt
+
+call crypto_env\Scripts\deactivate.bat
 
 @ECHO "Installed"
