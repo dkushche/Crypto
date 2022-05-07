90a80373bd92f1ce9f26b50788ebd46adf3d0593
Dmytro Kushchevskyi <d.kushchevskyi@sirinsoftware.com>
2022-05-07 17:39:51 +0300

    Check does next gen working

--- tests/algo/test_cryptoapi_aes.py
+++ tests/algo/test_cryptoapi_aes.py
@@ -13,5 +13,5 @@ from algo import cryptoapi_aes
 @pytest.mark.windows_set
 def test_cryptoapi_aes_success():
     assert cryptoapi_aes.processor(
-        bytearray("test", "utf-8"), 128, bytearray("test", "utf-8"), "CBC", "standard", "encrypt"
+        bytearray("test", "utf-8"), 128, bytearray("test", "utf-8"), "CBC", "nextgen", "encrypt"
     ) != f"Unsupported on {platform.system()} platform"
